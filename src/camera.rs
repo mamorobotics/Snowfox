4b8bc9e657af4b6d164dbe41b8628bdeb7c8d19a
maxjlaz1 <maxjlaz@icloud.com>
2024-05-31 16:34:51 -0400

    Camera Working?

--- src/camera.rs
+++ src/camera.rs
@@ -1,1 +1,10 @@
-use opencv::prelude::*;
+use opencv::{prelude::*, videoio::*};
+
+pub fn get_camera_buf(cam : &mut VideoCapture, cam_qual : i32) -> Vec<u8>{        
+        let mut frame = Mat::default();
+        cam.read(&mut frame);
+
+        let buf = Mat::data_bytes(&frame).unwrap();
+
+        return buf.to_vec();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 use std::str;
+use opencv::videoio::{self, VideoCapture};
 
 mod camera;
 
@@ -8,9 +9,14 @@ const PORT : &str = "8080";
 
 fn main() -> std::io::Result<()> {
     {
+        let mut cam_num = 0;
+        let mut cam_qual = 100;
+        let mut cam1 = videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap();
+        let mut cam2 = videoio::VideoCapture::new(1, videoio::CAP_ANY).unwrap();
+
         let socket = UdpSocket::bind(ADDR)?;
 
-        while true
+        loop
         {
             let mut init_buf = [0; 32];
             let (_amt, _src) = socket.recv_from(&mut init_buf)?;
@@ -32,15 +38,25 @@ fn main() -> std::io::Result<()> {
                 data_buf.append(&mut temp_buf);
             }
 
-            let _ = send_camera(&socket);
+            if *header == 6{
+                
+            }
+
+            let _ = send_camera(&socket, &mut cam1, &mut cam2, cam_num, cam_qual);
         }
     }
-    Ok(())
 }
 
-fn send_camera(socket : &UdpSocket) -> std::io::Result<()> {
+fn send_camera(socket : &UdpSocket, cam1 : &mut VideoCapture, cam2 : &mut VideoCapture, cam_num : i32, cam_qual : i32) -> std::io::Result<()> {
     {
-        let cam_buf = [0;0];
+        let cam_buf;
+
+        if cam_num==0{
+            cam_buf = camera::get_camera_buf(cam1, cam_qual);
+        }
+        else{
+            cam_buf = camera::get_camera_buf(cam2, cam_qual);
+        }
         socket.send_to(&cam_buf, SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 1, 1)), PORT.parse::<u16>().unwrap()))?;
     }
     Ok(())
